c27eaabaed95350741c7d2a370593e0fb8287bf1
Mike <ilmivy1024@gmail.com>
2021-03-20 02:18:31 +0600

    Initial Commit

---
+++ obj-c_Lesson1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  obj-c_Lesson1
+//
+//  Created by Michael Iliouchkin on 20.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
